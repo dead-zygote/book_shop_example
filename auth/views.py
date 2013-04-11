50fd948e54e2976467a293d4b3ee79720cfcab50
Anton <zygote.rb@gmail.com>
2013-04-11 19:33:19 +0700

    add views and templates for auth

--- auth/views.py
+++ auth/views.py
@@ -1,10 +1,48 @@
 # coding: utf-8
+from django.shortcuts import (
+    render,
+    redirect,
+    )
 
-def login(request):
-    pass
+from django.contrib import auth
+from django.contrib.auth.models import User
+from django.contrib import messages
+from cart.models import Cart
+from .forms import RegistrationForm
 
-def logout(request):
-    pass
 
 def register(request):
-    pass
+    if request.user.is_authenticated():
+        return redirect('/')
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            Cart.objects.create(user=user)
+            password = form.cleaned_data['password1']
+            user = auth.authenticate(username=user.username,
+                password=password)
+            auth.login(request, user)
+            return redirect('/')
+    else:
+        form = RegistrationForm()
+    return render(request, 'auth/registration.html', { 'form': form })
+
+
+def login(request):
+    if request.user.is_authenticated():
+        return redirect('/')
+    if request.method == 'POST':
+        user = auth.authenticate(username=request.POST['username'],
+            password=request.POST['password'])
+        if user and user.is_active:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.error(request, u'Вы неправильно ввели логин или пароль.')
+    return render(request, 'auth/login.html')
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
