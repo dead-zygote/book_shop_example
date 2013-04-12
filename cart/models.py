ba47dd3d9591e1addfb4c5888d04c0a0e4a44125
Anton <zygote.rb@gmail.com>
2013-04-12 23:15:18 +0700

    optimize Cart total_price property

--- cart/models.py
+++ cart/models.py
@@ -9,15 +9,23 @@ from django.db.models import (
 from django.core.validators import MinValueValidator
 from django.contrib.auth.models import User
 from catalogue.models import Book
+from django.db import connection
 
 
 class Cart(Model):
     user = OneToOneField(User, null=True)
 
+    @property
     def total_price(self):
-        items = self.items.select_related('book') \
-            .only('quantity', 'book__price')
-        return sum(item.book.price * item.quantity for item in items)
+        cursor = connection.cursor()
+        cursor.execute(
+            """
+            SELECT sum(cart_cartitem.quantity * catalogue_book.price)
+            FROM cart_cartitem INNER JOIN catalogue_book
+            ON cart_cartitem.book_id = catalogue_book.id
+            WHERE cart_cartitem.cart_id = %s
+            """, [self.id])
+        return cursor.fetchone()[0]
 
 
 class CartItem(Model):
